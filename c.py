e35a71def2b0c5f8f3efbdc7d0af9a1c974572d7
pajamas <444623630@qq.com>
2017-07-21 19:37:37 +0800

    v0.2 upadte client

--- c.py
+++ c.py
@@ -1,5 +1,13 @@
 import socket
+import thread
 s = socket.socket()
 s.connect(("192.168.1.31",50000))
-while True:
+
+def recv_message():
+  while True:
     print s.recv(1024)
+
+thread.start_new_thread(recv_message,())
+while True:
+  msg = str(socket.gethostname())+':' + raw_input("{}".format('someone>>:'))
+  s.sendall(msg) 
